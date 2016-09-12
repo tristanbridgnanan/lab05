d8ad2e7fe52f26a5a0ba53477f900d70a1dc1037
Branden Stone <bstone@adelphi.edu>
2016-09-12 19:05:40 -0400

    initial add

---
+++ lab05-person-creator/PersonCreator.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class PersonCreator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PersonCreator
+{
+    // Your class begins here!
+}
